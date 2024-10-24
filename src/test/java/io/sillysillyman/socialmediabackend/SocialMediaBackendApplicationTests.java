678a55e1c1baef1fe166a1c917da4a33ff4d72f7
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-25 03:46:04 +0900

    init commit

---
+++ src/test/java/io/sillysillyman/socialmediabackend/SocialMediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sillysillyman.socialmediabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
