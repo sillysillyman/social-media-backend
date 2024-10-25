fe7226c002f149a015c97ce3435ae141cca041ef
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-25 21:51:03 +0900

    feat: UserRepository 구현
    - existsByUsername 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package io.sillysillyman.socialmediabackend.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Boolean existsByUsername(String username);
+}
