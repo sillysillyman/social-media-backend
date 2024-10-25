7e13e8fcaa27077d2d5e2aac5f36fe1e9d7b1e75
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-25 11:32:36 +0900

    feat: 비밀번호 변경 요청 DTO 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/user/dto/ChangePasswordRequestDto.java
@@ -0,0 +1,18 @@
+package io.sillysillyman.socialmediabackend.domain.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class ChangePasswordRequestDto {
+
+    @NotBlank(message = "current password must not be blank")
+    String currentPassword;
+    @NotBlank(message = "new password must not be blank")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{8,}$",
+        message = "new password must contain at least one letter, one number, and one special character")
+    @Size(min = 8, message = "new password must be at least 8 characters long")
+    String newPassword;
+}
