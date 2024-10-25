5d6b10fe080d95417883b28118f3ed75e5695766
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-25 21:45:12 +0900

    feat: Post 엔티티 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/post/Post.java
@@ -0,0 +1,30 @@
+package io.sillysillyman.socialmediabackend.domain.post;
+
+import io.sillysillyman.socialmediabackend.common.BaseEntity;
+import io.sillysillyman.socialmediabackend.domain.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
