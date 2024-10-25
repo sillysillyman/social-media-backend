7c7aeafa1022357d6cbde62c06bc66b8f6eb2fd4
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-25 21:40:25 +0900

    feat: Comment 엔티티 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/comment/Comment.java
@@ -0,0 +1,35 @@
+package io.sillysillyman.socialmediabackend.domain.comment;
+
+import io.sillysillyman.socialmediabackend.common.BaseEntity;
+import io.sillysillyman.socialmediabackend.domain.post.Post;
+import io.sillysillyman.socialmediabackend.domain.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
