c4442896708f6816298d9634b1fe17cc32ccd76e
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-27 19:51:55 +0900

    feat: 정적 팩토리 메서드 추가

--- src/main/java/io/sillysillyman/socialmediabackend/common/dto/PagedListBody.java
+++ src/main/java/io/sillysillyman/socialmediabackend/common/dto/PagedListBody.java
@@ -1,13 +1,23 @@
 package io.sillysillyman.socialmediabackend.common.dto;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
 
 public record PagedListBody<T>(
-    List<T> data,
-    int currentPage,
+    List<T> content,
+    int number,
     int totalPages,
     int pageSize,
     long totalElements
 ) {
 
+    public static <T> PagedListBody<T> from(Page<T> page) {
+        return new PagedListBody<>(
+            page.getContent(),
+            page.getNumber(),
+            page.getTotalPages(),
+            page.getSize(),
+            page.getTotalElements()
+        );
+    }
 }
--- src/main/java/io/sillysillyman/socialmediabackend/common/dto/SingleItemBody.java
+++ src/main/java/io/sillysillyman/socialmediabackend/common/dto/SingleItemBody.java
@@ -2,4 +2,7 @@ package io.sillysillyman.socialmediabackend.common.dto;
 
 public record SingleItemBody<T>(T data) {
 
+    public static <T> SingleItemBody<T> from(T data) {
+        return new SingleItemBody<>(data);
+    }
 }
