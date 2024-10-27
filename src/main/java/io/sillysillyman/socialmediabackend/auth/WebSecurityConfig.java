88c3e9405034bd6a602ccfd984e3a5b0dfb626b7
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-27 15:54:36 +0900

    feat: SecurityFilterChain 임시 구현

--- src/main/java/io/sillysillyman/socialmediabackend/auth/WebSecurityConfig.java
+++ src/main/java/io/sillysillyman/socialmediabackend/auth/WebSecurityConfig.java
@@ -4,18 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
 @EnableWebSecurity
 public class WebSecurityConfig {
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(it -> it
+                .requestMatchers("**").permitAll()
+                .anyRequest().authenticated())
+            .formLogin(AbstractHttpConfigurer::disable).build();
     }
 }
