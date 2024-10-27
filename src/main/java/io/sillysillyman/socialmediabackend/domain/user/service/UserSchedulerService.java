56ea9f7000a176ad883250a8c1f01da8e7e73f6a
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-28 00:18:32 +0900

    refactor: deleteOldSoftDeletedUsers 구현
    - instant 이전의 soft deleted 사용자 삭제

--- src/main/java/io/sillysillyman/socialmediabackend/domain/user/service/UserSchedulerService.java
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/user/service/UserSchedulerService.java
@@ -1,8 +1,19 @@
 package io.sillysillyman.socialmediabackend.domain.user.service;
 
+import io.sillysillyman.socialmediabackend.domain.user.repository.UserRepository;
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class UserSchedulerService {
 
+    private final UserRepository userRepository;
+
+    @Transactional
+    public long deleteOldSoftDeletedUsers(Instant instant) {
+        return userRepository.deleteOlderThan(instant);
+    }
 }
