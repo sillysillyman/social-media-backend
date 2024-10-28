011cf4e02816edfa460f16ee6e9e1e850761ce36
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-29 01:29:15 +0900

    feat: findByUsername 추가

--- src/main/java/io/sillysillyman/socialmediabackend/domain/user/repository/UserRepository.java
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package io.sillysillyman.socialmediabackend.domain.user.repository;
 
 import io.sillysillyman.socialmediabackend.domain.user.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
     Boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 }
