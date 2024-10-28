b858c8e52935f07d011d5a9d9bd1aae51f068683
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-29 00:02:15 +0900

    feat: 메서드 추가
    - extractUsername: 토큰에서 username 추출하는 메서드
    - resolveToken: 토큰에서 Bearer prefix 제거
    - isTokenValid: 토큰 유효성 검증
    - isTokenExpired: 토큰 만료 검증
    - getClaims: 토큰에서 Claim 추출

--- src/main/java/io/sillysillyman/socialmediabackend/auth/JwtUtil.java
+++ src/main/java/io/sillysillyman/socialmediabackend/auth/JwtUtil.java
@@ -1,16 +1,23 @@
 package io.sillysillyman.socialmediabackend.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import io.sillysillyman.socialmediabackend.domain.user.UserRole;
 import java.security.Key;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-@Component
 @RequiredArgsConstructor
+@Slf4j(topic = "JwtUtil")
+@Component
 public class JwtUtil {
 
     public static final String AUTHORIZATION_HEADER = "Authorization";
@@ -23,12 +30,50 @@ public class JwtUtil {
     private final Key key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes());
 
     public String generateAccessToken(String username, UserRole role) {
-        return BEARER_PREFIX + generateToken(username, role,
-            jwtProperties.getAccessTokenExpiration());
+        return BEARER_PREFIX +
+            generateToken(username, role, jwtProperties.getAccessTokenExpiration());
     }
 
     public String generateRefreshToken(String username, UserRole role) {
         return generateToken(username, role, jwtProperties.getRefreshTokenExpiration());
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public String resolveToken(String token) {
+        return token.startsWith(BEARER_PREFIX) ? token.substring(BEARER_PREFIX.length()) : token;
+    }
+
+    public boolean isTokenValid(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT token: The token has expired. Token: {}", token);
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token: The token format is not supported. Token: {}", token);
+        } catch (MalformedJwtException e) {
+            log.error("Malformed JWT token: The token format is invalid. Token: {}", token);
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature: The token signature is invalid. Token: {}", token);
+        } catch (IllegalArgumentException e) {
+            log.error("Invalid JWT token: The token is either null or empty. Token: {}", token);
+        }
+        return false;
+    }
+
+    public boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(resolveToken(token))
+            .getBody();
     }
 
     private String generateToken(String username, UserRole role, long expiration) {
@@ -36,7 +81,7 @@ public class JwtUtil {
 
         return Jwts.builder()
             .setSubject(username)
-            .claim(AUTHORIZATION_KEY, role)
+            .claim(AUTHORIZATION_KEY, role.getAuthority())
             .setIssuedAt(now)
             .setExpiration(new Date(now.getTime() + expiration))
             .signWith(key, SIGNATURE_ALGORITHM)
