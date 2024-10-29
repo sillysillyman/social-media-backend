274ed65ced4cb5de29f4cfcf22d44d7b7d3c26c1
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-29 19:04:32 +0900

    feat: 정적 팩토리 메서드 of 추가

--- src/main/java/io/sillysillyman/socialmediabackend/common/dto/ErrorMessage.java
+++ src/main/java/io/sillysillyman/socialmediabackend/common/dto/ErrorMessage.java
@@ -2,4 +2,7 @@ package io.sillysillyman.socialmediabackend.common.dto;
 
 public record ErrorMessage(String detail, int status, String title) {
 
+    public static ErrorMessage of(String detail, int status, String title) {
+        return new ErrorMessage(detail, status, title);
+    }
 }
