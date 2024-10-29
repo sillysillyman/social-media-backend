17b7d856c7d083af2fb7fa0e12019b17d8a9597a
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-30 01:18:06 +0900

    feat: JwtConfig 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/auth/config/JwtConfig.java
@@ -0,0 +1,22 @@
+package io.sillysillyman.socialmediabackend.auth.config;
+
+import io.jsonwebtoken.security.Keys;
+import io.sillysillyman.socialmediabackend.auth.properties.JwtProperties;
+import java.security.Key;
+import java.util.Base64;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class JwtConfig {
+
+    private final JwtProperties jwtProperties;
+
+    @Bean
+    public Key key() {
+        byte[] bytes = Base64.getDecoder().decode(jwtProperties.getSecret());
+        return Keys.hmacShaKeyFor(bytes);
+    }
+}
