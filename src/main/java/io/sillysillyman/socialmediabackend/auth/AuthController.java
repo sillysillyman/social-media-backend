5aeef8a1c729cac596e74d837b5b0eb558e15c6d
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-30 02:20:22 +0900

    feat: AuthController 초기 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/auth/AuthController.java
@@ -0,0 +1,14 @@
+package io.sillysillyman.socialmediabackend.auth;
+
+import io.sillysillyman.socialmediabackend.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+}
