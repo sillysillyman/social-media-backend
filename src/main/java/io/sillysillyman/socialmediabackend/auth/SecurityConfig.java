1ae49f77dc90c153154139d084faddcc6a1d4428
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-30 00:46:17 +0900

    feat: SecurityConfig 추가 구현
    - 필드 추가
    - AuthenticationManager Bean 등록
    - SecurityFilterChain 에외 처리 핸들러 및 필터 추가

--- src/main/java/io/sillysillyman/socialmediabackend/auth/SecurityConfig.java
+++ src/main/java/io/sillysillyman/socialmediabackend/auth/SecurityConfig.java
@@ -3,20 +3,29 @@ package io.sillysillyman.socialmediabackend.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
 @RequiredArgsConstructor
+@Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -24,14 +33,30 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(AbstractHttpConfigurer::disable)
-            .sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(it -> it
-                .requestMatchers("**").permitAll()
-                .anyRequest().authenticated())
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
             .formLogin(AbstractHttpConfigurer::disable)
+            .exceptionHandling(it -> it
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+            )
+            .sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(
+                new JwtAuthenticationFilter(jwtUtil, userDetailsService)
+                , UsernamePasswordAuthenticationFilter.class
+            )
             .build();
     }
 }
