7bd957995345e714d9bb279e20aacf65f4da0a65
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-31 04:59:38 +0900

    feat: MethodArgumentNotValidException 핸들러 구현

--- src/main/java/io/sillysillyman/socialmediabackend/common/GlobalExceptionHandler.java
+++ src/main/java/io/sillysillyman/socialmediabackend/common/GlobalExceptionHandler.java
@@ -2,8 +2,13 @@ package io.sillysillyman.socialmediabackend.common;
 
 import io.sillysillyman.socialmediabackend.common.dto.ErrorMessage;
 import io.sillysillyman.socialmediabackend.domain.user.exception.UserException;
+import java.util.List;
+import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,6 +24,28 @@ public class GlobalExceptionHandler {
                 e.getUserErrorCode().getMessage(),
                 e.getUserErrorCode().getStatus().value(),
                 e.getUserErrorCode().getStatus().name()
+            )
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e
+    ) {
+        List<String> errorMessages = Stream.concat(
+                e.getBindingResult().getFieldErrors().stream(),
+                e.getBindingResult().getGlobalErrors().stream()
+            )
+            .map(ObjectError::getDefaultMessage)
+            .toList();
+
+        String combinedErrorMessage = String.join("\n", errorMessages);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            new ErrorMessage(
+                combinedErrorMessage,
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.name()
             )
         );
     }
