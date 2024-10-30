e324677e464799053ef3548824042d307811af81
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-31 02:00:20 +0900

    feat: AuthErrorCode 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/auth/exception/AuthErrorCode.java
@@ -0,0 +1,15 @@
+package io.sillysillyman.socialmediabackend.auth.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCode {
+    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "username or password mismatch"),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "invalid token");
+
+    private final HttpStatus status;
+    private final String message;
+}
