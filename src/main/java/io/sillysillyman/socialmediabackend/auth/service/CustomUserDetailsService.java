6b707da7a99f31d6c05f6e19fd63e3b8657391a9
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-30 13:51:26 +0900

    fix: 순환 의존성 문제로 UserRepository 주입

--- src/main/java/io/sillysillyman/socialmediabackend/auth/service/CustomUserDetailsService.java
+++ src/main/java/io/sillysillyman/socialmediabackend/auth/service/CustomUserDetailsService.java
@@ -1,8 +1,9 @@
 package io.sillysillyman.socialmediabackend.auth.service;
 
 import io.sillysillyman.socialmediabackend.auth.CustomUserDetails;
+import io.sillysillyman.socialmediabackend.domain.user.exception.UserErrorCode;
 import io.sillysillyman.socialmediabackend.domain.user.exception.detail.UserNotFoundException;
-import io.sillysillyman.socialmediabackend.domain.user.service.UserService;
+import io.sillysillyman.socialmediabackend.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,12 +14,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserService userService;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         try {
-            return new CustomUserDetails(userService.getByUsername(username));
+            return new CustomUserDetails(
+                userRepository.findByUsername(username).orElseThrow(() ->
+                    new UserNotFoundException(UserErrorCode.USER_NOT_FOUND)
+                )
+            );
         } catch (UserNotFoundException e) {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
