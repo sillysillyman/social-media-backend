f2182de08d91abbc69749f743bf5421b60b75849
sillysillyman <sillysillyman.cs@gmail.com>
2024-10-30 13:50:33 +0900

    feat: 세터 추가

--- src/main/java/io/sillysillyman/socialmediabackend/auth/properties/JwtProperties.java
+++ src/main/java/io/sillysillyman/socialmediabackend/auth/properties/JwtProperties.java
@@ -1,9 +1,11 @@
 package io.sillysillyman.socialmediabackend.auth.properties;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
+@Setter
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
 
