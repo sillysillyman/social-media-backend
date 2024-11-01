c8b8fd348b780e655882fe74ece801f833307faa
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-01 11:41:54 +0900

    chore: 불필요한 애너테이션 제거

--- src/main/java/io/sillysillyman/socialmediabackend/domain/user/repository/UserRepository.java
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/user/repository/UserRepository.java
@@ -3,9 +3,7 @@ package io.sillysillyman.socialmediabackend.domain.user.repository;
 import io.sillysillyman.socialmediabackend.domain.user.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
     Boolean existsByUsername(String username);
