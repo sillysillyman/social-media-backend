b462abf0b3fdbbe564e18ba3bb0e465f9179a826
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-01 16:34:48 +0900

    feat: Post 수정 요청 DTO 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/post/dto/UpdatePostRequest.java
@@ -0,0 +1,15 @@
+package io.sillysillyman.socialmediabackend.domain.post.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UpdatePostRequest {
+
+    private String content;
+
+    // TODO: 삭제할 이미지 ID 목록
+    // private List<Long> deleteImageIds;
+
+    // TODO: 추가할 새 이미지 목록
+    // private List<MultipartFile> newImages;
+}
