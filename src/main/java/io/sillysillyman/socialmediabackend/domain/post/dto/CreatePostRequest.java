975f93e63ccc674450ad76879ecaa75472d26d4d
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-01 16:34:32 +0900

    feat: Post 생성 요청 DTO 구현

---
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/post/dto/CreatePostRequest.java
@@ -0,0 +1,17 @@
+package io.sillysillyman.socialmediabackend.domain.post.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
+
+
+@Getter
+public class CreatePostRequest {
+
+    @NotBlank(message = "content must not be blank")
+    @Length(max = 500, message = "content must be at most 500 characters")
+    private String content;
+
+    // TODO: 업로드 이미지 목록
+    // List<MultipartFile> images;
+}
