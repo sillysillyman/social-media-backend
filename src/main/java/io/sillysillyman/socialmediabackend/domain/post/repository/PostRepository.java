56c795d3010880783ac3afb4840a1b9e3780c3df
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-02 03:35:35 +0900

    feat: 사용자 ID로 Post 페이지 조회 메서드 추가

--- src/main/java/io/sillysillyman/socialmediabackend/domain/post/repository/PostRepository.java
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/post/repository/PostRepository.java
@@ -1,8 +1,11 @@
 package io.sillysillyman.socialmediabackend.domain.post.repository;
 
 import io.sillysillyman.socialmediabackend.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    Page<Post> findByUserId(Long userId, Pageable pageable);
 }
