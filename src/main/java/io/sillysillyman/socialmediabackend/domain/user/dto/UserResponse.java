716cbae8311e975595745ae39ad96bf5e044bffc
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-04 01:45:59 +0900

    rename: UserResponse 필드명 변경

--- src/main/java/io/sillysillyman/socialmediabackend/domain/user/dto/UserResponse.java
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/user/dto/UserResponse.java
@@ -2,7 +2,7 @@ package io.sillysillyman.socialmediabackend.domain.user.dto;
 
 import io.sillysillyman.socialmediabackend.domain.user.User;
 
-public record UserResponse(Long id, String username) {
+public record UserResponse(Long userId, String username) {
 
     public static UserResponse from(User user) {
         return new UserResponse(user.getId(), user.getUsername());
