7e5de07a4afc28f67821090b4fba4a8db358f9e3
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-04 02:41:26 +0900

    feat: getter 애너테이션 추가

--- src/main/java/io/sillysillyman/socialmediabackend/domain/comment/dto/UpdateCommentRequest.java
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/comment/dto/UpdateCommentRequest.java
@@ -1,7 +1,9 @@
 package io.sillysillyman.socialmediabackend.domain.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 
+@Getter
 public class UpdateCommentRequest {
 
     @NotBlank(message = "content must not be blank")
