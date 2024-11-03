0f0288c490521ce906c041e7a0f1864d138737d8
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-04 03:36:51 +0900

    feat: Comment 업데이트 로직 구현
    - Comment 작성자 검증 로직 구현
    - Comment와 Post 포함 관계 검증 로직 구현

--- src/main/java/io/sillysillyman/socialmediabackend/domain/comment/service/CommentService.java
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/comment/service/CommentService.java
@@ -1,14 +1,19 @@
 package io.sillysillyman.socialmediabackend.domain.comment.service;
 
+import io.sillysillyman.socialmediabackend.auth.exception.AuthErrorCode;
+import io.sillysillyman.socialmediabackend.auth.exception.detail.UnauthorizedAccessException;
 import io.sillysillyman.socialmediabackend.domain.comment.Comment;
 import io.sillysillyman.socialmediabackend.domain.comment.dto.CommentResponse;
 import io.sillysillyman.socialmediabackend.domain.comment.dto.CreateCommentRequest;
+import io.sillysillyman.socialmediabackend.domain.comment.dto.UpdateCommentRequest;
 import io.sillysillyman.socialmediabackend.domain.comment.exception.CommentErrorCode;
+import io.sillysillyman.socialmediabackend.domain.comment.exception.detail.CommentNotBelongToPostException;
 import io.sillysillyman.socialmediabackend.domain.comment.exception.detail.CommentNotFoundException;
 import io.sillysillyman.socialmediabackend.domain.comment.repository.CommentRepository;
 import io.sillysillyman.socialmediabackend.domain.post.Post;
 import io.sillysillyman.socialmediabackend.domain.post.service.PostService;
 import io.sillysillyman.socialmediabackend.domain.user.User;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -48,5 +53,32 @@ public class CommentService {
     @Transactional(readOnly = true)
     public Page<CommentResponse> getComments(Long postId, Pageable pageable) {
         return commentRepository.findByPostId(postId, pageable).map(CommentResponse::from);
+    }
+
+    @Transactional
+    public void updateComment(
+        Long postId,
+        Long commentId,
+        UpdateCommentRequest updateCommentRequest,
+        User user
+    ) {
+        Comment comment = getById(commentId);
+
+        validateCommentOwnership(user.getId(), comment.getUser().getId());
+        validateCommentPostId(postId, comment.getPost().getId());
+
+        comment.update(updateCommentRequest);
+    }
+
+    private void validateCommentOwnership(Long userId, Long authorId) {
+        if (Objects.equals(authorId, userId)) {
+            throw new UnauthorizedAccessException(AuthErrorCode.UNAUTHORIZED_ACCESS);
+        }
+    }
+
+    private void validateCommentPostId(Long requestPostId, Long commentPostId) {
+        if (!Objects.equals(requestPostId, commentPostId)) {
+            throw new CommentNotBelongToPostException(CommentErrorCode.COMMENT_NOT_BELONG_TO_POST);
+        }
     }
 }
