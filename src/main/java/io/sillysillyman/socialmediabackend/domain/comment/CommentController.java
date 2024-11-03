e2a96cf709f4f7d719cb09e233b67e6484c055cb
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-03 23:58:30 +0900

    refactor: API 엔드포인트 URI 리소스 계층 구조 구체화
    - 공통 매핑 경로에 Post 관련 상위 경로 추가

--- src/main/java/io/sillysillyman/socialmediabackend/domain/comment/CommentController.java
+++ src/main/java/io/sillysillyman/socialmediabackend/domain/comment/CommentController.java
@@ -11,13 +11,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/comments")
+@RequestMapping("/api/v1/posts/{postId}/comments")
 @RestController
 public class CommentController {
 
@@ -25,9 +26,14 @@ public class CommentController {
 
     @PostMapping
     public ResponseEntity<SingleItemResponse<CommentResponse>> createComment(
+        @PathVariable Long postId,
         @Valid @RequestBody CreateCommentRequest createCommentRequest,
         @AuthenticationPrincipal CustomUserDetails userDetails
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(SingleItemResponse.from(null));
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            SingleItemResponse.from(
+                commentService.createComment(postId, createCommentRequest, userDetails.user())
+            )
+        );
     }
 }
