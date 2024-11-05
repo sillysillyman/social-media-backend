56bb4cb1ae8a16d30160339fb92d3d788e5ee544
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-06 01:14:50 +0900

    feat: 애너테이션 추가
    - EntityScan: 패키지 내의 엔티티 스캔
    - ComponentScan: 패키지 내의 빈 스캔
    - EnableJpaRepositories: 패키지 내의 JpaRepository 허용

--- api/src/main/java/io/sillysillyman/api/ApiApplication.java
+++ api/src/main/java/io/sillysillyman/api/ApiApplication.java
@@ -2,15 +2,17 @@ package io.sillysillyman.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "io.sillysillyman.core.domain")
 @ComponentScan(basePackages = {
     "io.sillysillyman.api",
     "io.sillysillyman.core"
 })
-@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "io.sillysillyman.core.domain")
 public class ApiApplication {
 
     public static void main(String[] args) {
