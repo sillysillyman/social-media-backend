79b080a732f4161fba8e55ac64e452966b2d473c
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-05 20:49:44 +0900

    fix: core 모듈 빈 의존성 주입

--- api/src/main/java/io/sillysillyman/api/ApiApplication.java
+++ api/src/main/java/io/sillysillyman/api/ApiApplication.java
@@ -2,9 +2,14 @@ package io.sillysillyman.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "io.sillysillyman.api",
+    "io.sillysillyman.core"
+})
 @EnableJpaAuditing
 public class ApiApplication {
 
