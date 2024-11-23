f6274a4a0b15e80ffdcb0010ac39190a49eedc5f
sillysillyman <sillysillyman.cs@gmail.com>
2024-11-24 00:46:43 +0900

    feat: Reply 관련 예외 추가
    - 댓글에 속하지 않은 대댓글 요청 시 예외 추가

--- core/src/main/java/io/sillysillyman/core/domain/reply/exception/ReplyErrorCode.java
+++ core/src/main/java/io/sillysillyman/core/domain/reply/exception/ReplyErrorCode.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ReplyErrorCode {
+    REPLY_NOT_BELONG_TO_COMMENT(HttpStatus.BAD_REQUEST, "reply does not belong to the comment"),
     REPLY_NOT_FOUND(HttpStatus.NOT_FOUND, "reply not found");
-    
+
     private final HttpStatus status;
     private final String message;
 }
